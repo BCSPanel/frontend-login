c85add315aed552e25ce3e554acc0fa2a683db71
bddjr <bddjrcn@outlook.com>
2024-02-10 23:18:27 +0800

    调整加密模块

--- src/clickLogin_noSecure_encrypt.ts
+++ src/clickLogin_noSecure_encrypt.ts
@@ -35,22 +35,23 @@ export async function updateBody(postLoginBody: postLoginBodyType, login_salt_ap
     if (isRegister) {// 是注册
         // 读取验证码
         const verification_code = (document.getElementById("verification_code") as HTMLInputElement).value.trim();
-        // 将验证码作为盐，加盐并再次不可逆加密
-        postLoginBody.password = await SHA256(postLoginBody.password + verification_code);
+        // GeXcNE52YEcVuqOz
+
         // 验证码与不可逆加密后的密码混合，不可逆加密后再发送（密码解密后符合sha256特征，那么使用这个进行第2步验证）
         postLoginBody.salt = await SHA256(verification_code + postLoginBody.password);
         // 导入crypto-js
         const { encrypt } = (await import('./tripledes_encrypt'));
         // 使用验证码作为密钥，对称加密
         postLoginBody.password = encrypt(postLoginBody.password, verification_code);
+
     } else {// 是登录
         // 向服务器要盐
         const response = await fetchWithTimeout(login_salt_api.replace('{{username}}', postLoginBody.username));
         if (!response.ok) throw response.status;
 
-        /** ["该用户注册时的验证码", "随机盐"] */
+        /** ["服务器记录密码时使用的盐绝对不能直接使用注册时的验证码", "随机盐"] */
         const someSalt = await response.json();
-        // const someSalt = ["114514","1919810"]
+        // const someSalt = ["sv0HfJPHJhdL6K7X","MnNYEgjZDjoTiCVq"]
 
         // 加盐并再次不可逆加密
         for (const i of someSalt) {
