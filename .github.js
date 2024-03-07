617866366594e9a18b63443cc67c3d617eb715ee
bddjr <bddjrcn@outlook.com>
2024-03-07 22:33:37 +0800

    ghpages

--- .github.js
+++ .github.js
@@ -1,8 +1,23 @@
 console.log(".github.js");
 import fs from "fs";
 
-fs.mkdirSync("./dist/api-login", { recursive: true });
+fs.mkdirSync("./dist-ghpages/api-login", { recursive: true });
+fs.mkdirSync("./dist-ghpages/web", { recursive: true });
+fs.renameSync('./dist/','./dist-ghpages/web-login/')
+
 fs.writeFileSync(
-  "./dist/api-login/salt",
+  "./dist-ghpages/index.html",
+  '<script> location.pathname = "/web-login/" </script>'
+);
+fs.writeFileSync(
+  "./dist-ghpages/web/index.html",
+  '<script> location.pathname = "/web-login/" </script>'
+);
+fs.writeFileSync(
+  "./dist-ghpages/api-login/salt",
   '["sv0HfJPHJhdL6K7X","MnNYEgjZDjoTiCVq"]'
+);
+fs.writeFileSync(
+  "./dist-ghpages/api-login/color-scheme.js",
+  'window.BCSPanelColorScheme="";window.matchMediaDarkChange?.()'
 );
