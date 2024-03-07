5021d34210e59e83245cd06b966fe2aac1e9e2ce
bddjr <bddjrcn@outlook.com>
2024-03-07 22:42:50 +0800

    ghpages

--- .github.js
+++ .github.js
@@ -1,23 +1,6 @@
-console.log(".github.js");
+console.log("Running .github.js");
 import fs from "fs";
 
-fs.mkdirSync("./dist-ghpages/api-login", { recursive: true });
-fs.mkdirSync("./dist-ghpages/web", { recursive: true });
 fs.renameSync('./dist/','./dist-ghpages/web-login/')
 
-fs.writeFileSync(
-  "./dist-ghpages/index.html",
-  '<script> location.pathname = "/web-login/" </script>'
-);
-fs.writeFileSync(
-  "./dist-ghpages/web/index.html",
-  '<script> location.pathname = "/web-login/" </script>'
-);
-fs.writeFileSync(
-  "./dist-ghpages/api-login/salt",
-  '["sv0HfJPHJhdL6K7X","MnNYEgjZDjoTiCVq"]'
-);
-fs.writeFileSync(
-  "./dist-ghpages/api-login/color-scheme.js",
-  'window.BCSPanelColorScheme="";window.matchMediaDarkChange?.()'
-);
+console.log('ok');
