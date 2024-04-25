d3fe942a96fb0935e8db121a2bca6f6575482aa0
bddjr <bddjrcn@outlook.com>
2024-04-25 15:49:58 +0800

    更改loginConfig.ts

--- src/loginConfig.ts
+++ src/loginConfig.ts
@@ -1,9 +1,6 @@
 export default {
     login_api: '../api-login/login',
-    login_salt_api(username: string) {
-        return `../api-login/salt?username=${username}`
-    },
     async login_success_redirect() {
-        document.location.replace('../web/')
+        document.location.replace('../')
     },
 }
